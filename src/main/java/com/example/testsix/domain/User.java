d91623d00460c164cd058ecca88635d7ebb56fbe
EvgeniyD1 <edolgiyj7@gmail.com>
2022-08-01 16:04:12 +0300

    Initial commit

---
+++ src/main/java/com/example/testsix/domain/User.java
@@ -0,0 +1,14 @@
+package com.example.testsix.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Long number;
+    private String id;
+    private String username;
+    private String address;
+    private String phoneNumber;
+
+}
