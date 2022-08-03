16ef119789542c9d6cde95c444c3fb00ec2ee3d9
EvgeniyD1 <edolgiyj7@gmail.com>
2022-08-03 21:38:56 +0300

    slider + scroll

--- src/main/resources/js/App.vue
+++ src/main/resources/js/App.vue
@@ -2,13 +2,22 @@
   <v-app>
     <v-app-bar>
       <v-toolbar-title>TestSixApplication</v-toolbar-title>
-      <div>
-        {{ type }}
-      </div>
+      <v-spacer></v-spacer>
       <v-btn @click="getRuUsers">RU</v-btn>
       <v-btn @click="getUaUsers">UA</v-btn>
       <v-btn @click="getUsUsers">US</v-btn>
+
+      <template v-slot:extension>
+        <v-slider
+            v-model="errors"
+            :min="0"
+            :max="10"
+            :step="1"
+            thumb-label
+        ></v-slider>
+      </template>
     </v-app-bar>
+
     <v-table
         fixed-header
         v-if="records.length!==0"
@@ -32,6 +41,8 @@
       </tr>
       </tbody>
     </v-table>
+
+    <div ref="observer"></div>
   </v-app>
 </template>
 
@@ -42,31 +53,68 @@ export default {
   data() {
     return {
       records: [],
-      type: ''
+      type: '',
+      errors: 0
     }
   },
   methods: {
-    async getRuUsers(){
-      let url = '/ru';
+    async getRuUsers() {
+      if (this.type!=='ru') this.records = [];
+      let url = '/ru?errors=' + this.errors;
+      this.type = 'ru';
       const response = await axios.get(url);
-      this.records = response.data;
+      this.records = [...this.records, ...response.data];
     },
-    async getUaUsers(){
-      let url = '/ua';
+    async getUaUsers() {
+      if (this.type!=='ua') this.records = [];
+      let url = '/ua?errors=' + this.errors;
+      this.type = 'ua';
       const response = await axios.get(url);
-      this.records = response.data;
+      this.records = [...this.records, ...response.data];
     },
-    async getUsUsers(){
-      let url = '/us';
+    async getUsUsers() {
+      if (this.type!=='us') this.records = [];
+      let url = '/us?errors=' + this.errors;
+      this.type = 'us';
       const response = await axios.get(url);
-      this.records = response.data;
+      this.records = [...this.records, ...response.data];
     }
+  },
+  // mounted() {
+  //   window.onscroll = () => {
+  //     const el = document.documentElement;
+  //     const isBottomOfScreen = el.scrollTop + window.innerHeight === el.offsetHeight;
+  //     if (isBottomOfScreen) {
+  //       console.log(isBottomOfScreen)
+  //       if (this.type==='ru') this.getRuUsers();
+  //       if (this.type==='ua') this.getUaUsers();
+  //       if (this.type==='us') this.getUsUsers();
+  //     }
+  //   }
+  // },
+  // beforeUnmount() {
+  //   window.onscroll = null
+  // }
+  mounted() {
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting){
+        if (this.type==='ru') this.getRuUsers();
+        if (this.type==='ua') this.getUaUsers();
+        if (this.type==='us') this.getUsUsers();
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
   }
 }
 </script>
 
 <style>
 body {
-  padding-top: 70px;
+  padding-top: 120px;
 }
 </style>
